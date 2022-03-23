a79be023344ed6f8e4844596703a8a457d937fed
UtasiB <80397148+UtasiB@users.noreply.github.com>
2022-03-23 13:06:16 +0100

    Python tkinter update

--- python/feladat2.py
+++ python/feladat2.py
@@ -1,20 +1,22 @@
 from tkinter import *
 import math
 foablak=Tk()
-def térfogat():
-    a = int(mezo1.get())
-    b = int(mezo2.get())
+
+def szamit():
+    a = float(mezo1.get())
+    b = float(mezo2.get())
     V = a**2 * b * math.pi
     mezo3.delete(0, END)
-    mezo3.insert(0, ''+str(V))
-def vashenger():
-    a = int(mezo1.get())
-    b = int(mezo2.get())
-    V = a**2 * b * math.pi
-    p = 2700
+    mezo3.insert(0, ''+str("%.2f" % V)+ ' cm3')
+    p = 7.8
     m = p * V
     mezo4.delete(0, END)
-    mezo4.insert(0, ''+str(m))
+    mezo4.insert(0, ''+str("%.2f" % m)+' g')
+    p2 = 0.7
+    m2 = p2 * V
+    mezo5.delete(0, END)
+    mezo5.insert(0, ''+str("%.2f" % m2)+' g')
+
 szoveg1=Label(foablak, text='Sugár (cm):')
 szoveg1.grid(row=0,column=0)
 mezo1=Entry(foablak)
@@ -23,8 +25,8 @@ szoveg2=Label(foablak, text='Magasság (cm):')
 szoveg2.grid(row=1,column=0)
 mezo2=Entry(foablak)
 mezo2.grid(row=1,column=1,)
-gomb = Button(foablak, text='Kiszámít', command=lambda:[térfogat(), vashenger()])
-gomb.grid(row=2,column =1)
+gomb = Button(foablak, text='Kiszámít', command=szamit)
+gomb.grid(row=2,column =2,)
 szoveg3=Label(foablak, text='Térfogat:')
 szoveg3.grid(row=3,column=0)
 mezo3=Entry(foablak)
